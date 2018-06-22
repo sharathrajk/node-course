164a3e9e79947da2ce99af8a5c21b7a3a3091784
sharathrajk <sharath.rajk@htmtechsys.com>
2018-06-22 16:21:40 +0530

    Add connect and didconnet

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const publicPath = path.join(__dirname,'../public');
 //console.log(__dirname+ '/../public');
 //console.log(publicPath);
+const socketIO = require('socket.io');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
+io.on('connection', (socket)=>{
+console.log('new user connected');
+
+socket.on('disconnect',()=>{
+    console.log('new user disconnected');
+});
+});
+
+
+
+server.listen(port, ()=>{
     console.log(`server is running in ${port}`);
 });
